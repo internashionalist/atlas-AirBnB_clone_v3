988f002bd5d0d82ed8eb6c394fb6f387c7d72e1a
Nash Thames <taylor.thames@atlasschool.com>
2024-11-10 14:59:55 -0600

    was missing imports, calling classes as strings

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ This module contains the status and stats routes.
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
@@ -15,18 +22,16 @@ def status():
 @app_views.route("/stats")
 def stats():
     """ returns the STATS of the API """
-    from models import storage
-
     classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User",
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User,
     }
 
     stats_dict = {}
-    for key, value in classes.items():
-        stats_dict[key] = storage.count(value)
+    for key, cls in classes.items():
+        stats_dict[key] = storage.count(cls)
     return jsonify(stats_dict)
