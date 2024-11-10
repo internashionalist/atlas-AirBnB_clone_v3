9c40702577f9420f8104798bc9c0b024f8276f70
Nash Thames <taylor.thames@atlasschool.com>
2024-11-10 09:45:11 -0600

    blueprint registration

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,8 @@ import os
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix="/api/v1")
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
