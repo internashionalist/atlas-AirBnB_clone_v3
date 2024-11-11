aaeb133843d78e225cb092eb856f51fdb9c89738
Nash Thames <taylor.thames@atlasschool.com>
2024-11-10 18:08:29 -0600

    PEP8

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -85,6 +85,7 @@ class TestFileStorageDocs(unittest.TestCase):
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
 
+
 class TestFileStorage(unittest.TestCase):
     """
     tests the FileStorage class
